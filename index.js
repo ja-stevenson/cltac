f2ffc80df7cefbabfc76d9414f93d410438e6e2c
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-01-16 15:07:03 -0600

    basic logic for adding moves included

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
 var prompt = require('prompt');
 
+prompt.start();
+
 const TicGame = function() {
-  let board = [[null,null,null],[null,null,null],[null,null,null]]
-  let turn = true;
+  this._board = [[null,null,null],[null,null,null],[null,null,null]]
+  this._turn = true;
 }
 
+let game = TicGame();
+
+TicGame.prototype.addMove = function(options){
+  if(this._turn){
+    this._board[options.column][options.row] = 'X';
+    console.log('Player 2\'s turn');
+  } else {
+    this._board[options.column][options.row] = 'O';
+    console.log('Player 1\'s turn');
+  }
+}
+
+prompt.get(['column','row'], function(err, result){
+  game.addMove(result);
+});
