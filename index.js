28f99b3f55d861349f3ebafc3d2462f876c9ae5b
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-01-16 15:21:38 -0600

    stubs for winnerChecker created

--- index.js
+++ index.js
@@ -20,6 +20,32 @@ TicGame.prototype.addMove = function(options){
   }
 }
 
+TicGame.prototype.checkWinner = function(){
+  function checkHorizontal(){
+    for(var i = 0; i < this._board.length; i++){
+      if(this._board[i][0] === this._board[i][1] && this._board[i][1] === this._board[i][2]){
+        return this._board[i][0] === 'X'
+          ? {win: true, player: 'X'}
+          : {win: true, player: 'O'}
+      } else {
+        return false;
+      }
+    }
+  };
+  function checkVertical(){
+    for(var i = 0; i < this._board.length; i++){
+      
+    }
+  };
+  function checkDiagonal(){
+
+  };
+
+  if(checkHorizontal() || checkVertical() || checkDiagonal()){
+    return 'Winner winner';
+  }
+}
+
 prompt.get(['column','row'], function(err, result){
   game.addMove(result);
 });
